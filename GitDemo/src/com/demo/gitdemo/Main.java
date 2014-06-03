5c6b3f19dd31363688f8151a65b1084a2cb20a1e
ijazahmed <ijaxahmed@gmail.com>
2014-06-03 09:04:38 +0200

    First Chnage: Simply a new Output line

--- GitDemo/src/com/demo/gitdemo/Main.java
+++ GitDemo/src/com/demo/gitdemo/Main.java
@@ -13,6 +13,9 @@ public class Main {
 	public static void displayGreeting(){
 		System.out.println("HELLO HAPPY SALES PEOPLE");
 		System.out.println("THIS APP SHOWS SALES DATA");
+		
+		System.out.println("First Change");
+		
 	}
 
 }
